cec3054395f3d8ad54abaa541858040d80ca96bb
Raquel Alonso <raque13@hotmail.com>
2023-11-09 10:20:04 -0300

    arreglando el select propiedades

--- src/Propiedad.jsx
+++ src/Propiedad.jsx
@@ -2,26 +2,36 @@ import { useEffect } from "react";
 import { useState } from "react";
 
 function Propiedad() {
-  const [propiedad, setPropiedad] = useState({});
+  const [propiedad, setPropiedad] = useState([]);
+  const [selectedOption, setSelectedOption] = useState('');
 
   useEffect(() => {
     fetch("https://653831aaa543859d1bb14d53.mockapi.io/propiedades")
       .then((res) => res.json())
-      .then((prop) => setPropiedad(prop));
+      .then((info) => 
+      setPropiedad(info));
   }, []);
+
+  const handleChange = (event) => {
+    setSelectedOption(event.target.value);
+  };
 
   const handleSubmit = (e) => {
     e.preventDefault();
   };
+ 
 
   return (
     <form onSubmit={handleSubmit}>
-      <label>Seleccione el tipo de propiedad</label>
-      <br />
-      <select>{propiedad.tipo}</select>
-      <br />
-      <h2>{propiedad.id}</h2>
-      <button type="submit">Cotizar</button>
+      <label htmlFor="selectOption" >Selecciona un tipo de propiedad:</label>
+      <select id="selectOption" value={selectedOption} onChange={handleChange} >
+       <option></option>
+       {propiedad.map ((propiedad) =>(
+          <option key={propiedad.id} value={propiedad.id}>{propiedad.tipo}</option>
+        ))}
+      </select>
+       
+      <button type="submit">Enviar</button>
     </form>
   );
 }
